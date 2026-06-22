33688f645d442d6811918a66260d5e494dd6057f
Bohdan <slutskybo@gmail.com>
2026-06-22 17:13:37 +0200

    Add Python 52 homework

---
+++ 52__Python 52 Основы работы с файлами/homework/homework_01.py
@@ -0,0 +1,50 @@
+""" 01 Фильтрация по ключевому слову
+
+Напишите программу, которая
+- ищет в файле все строки, содержащие указанное пользователем слово,
+- и сохраняет их в новый файл.
+
+Имя нового файла формируется как <keyword>_<original_filename>.
+Если файл не существует, программа должна вывести ошибку.
+Если совпадения не найдены, новый файл не создаётся.
+
+Используйте файл system_log.txt.
+
+Пример ввода:
+Введите имя файла для поиска: system_log.txt
+Введите ключевое слово: error
+
+Пример вывода:
+Строки, содержащие 'error', сохранены в <keyword>_<original_filename>.
+
+"""
+
+def find_keyword(filename: str, keyword: str) -> None:
+    try:
+        with open(filename, 'r') as file:
+            found_lines = []
+
+            for line in file:
+                if keyword in line:
+                    found_lines.append(line)
+    except FileNotFoundError as error:
+        print(f'File not found: {error}')
+        return
+
+    if not found_lines:
+        return
+
+    new_filename = f'{keyword}_{filename}'
+
+    with open(new_filename, 'w') as file:
+        for line in found_lines:
+            file.write(line)
+
+    print(f"Строки, содержащие '{keyword}', сохранены в {new_filename}.")
+
+
+find_keyword('s', 'error')
+# File not found: [Errno 2] No such file or directory: 's'
+
+find_keyword('system_log.txt', 'error')
+# Строки, содержащие 'error', сохранены в error_system_log.txt.
---
+++ 52__Python 52 Основы работы с файлами/homework/homework_02.py
@@ -0,0 +1,52 @@
+""" 02 Поиск и удаление дубликатов
+
+Напишите программу, которая
+- удаляет дублирующиеся строки из файла
+- и сохраняет результат в новый файл.
+
+Имя нового файла формируется как unique_<original_filename>.
+
+Если файл не существует, программа должна вывести ошибку.
+
+Исходный порядок строк должен сохраниться.
+Если в файле нет дубликатов, создаётся точная копия файла.
+
+Используйте файл movies_to_watch.txt.
+
+Пример ввода:
+Введите имя файла: movies_to_watch.txt
+
+Пример вывода:
+Дубликаты удалены. Уникальные строки сохранены в unique_movies_to_watch.txt.
+
+"""
+
+def remove_duplicates(filename: str) -> None:
+    try:
+        with open(filename, 'r') as file:
+            unique_lines = []
+            seen_lines = []
+
+            for line in file:
+                if line not in seen_lines:
+                    seen_lines.append(line)
+                    unique_lines.append(line)
+    except FileNotFoundError as error:
+        print(f'File not found: {error}')
+        return
+
+    new_filename = f'unique_{filename}'
+
+    with open(new_filename, 'w') as file:
+        for line in unique_lines:
+            file.write(line)
+
+    print(f'Дубликаты удалены. Уникальные строки сохранены в {new_filename}.')
+
+
+
+remove_duplicates("movies_to_watch.txt")
+# Дубликаты удалены. Уникальные строки сохранены в unique_movies_to_watch.txt.
+
+remove_duplicates("M")
+# File not found: [Errno 2] No such file or directory: 'M'
